a0d4f8779bd1e43508ff766b9be7283881ffca64
Sergey Minaev <CARman08@yandex.ru>
2024-12-22 18:16:12 +0300

    check
    Проверка подключения к GitHub + проверка работы OpenCV

---
+++ superResolution.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+
+print("Super-Resolution from a single image")
+
+print("Image openning...")
+img = cv.imread("lena.jpg")
+cv.imshow("LENA", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+print("Image was closed!")
